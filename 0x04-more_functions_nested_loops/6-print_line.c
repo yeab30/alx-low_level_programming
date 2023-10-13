40ca10af789a3a7eaa53499e0b7a9af274695616
yeab30 <yeabsirabeletengu@gmail.com>
2023-10-13 21:36:50 +0300

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - Draws a straight line according to parameter 
+ * @n: the number of line to draw
+ * Return: empty
+ */
+void print_line(int n)
+{
+	int x;
+	if (n <=0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (x = 0; x < n; x++)
+	{
+	_putchar(95);
+	}
+	_putchar('\n');
+	}
+}
