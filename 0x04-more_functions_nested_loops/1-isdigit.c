eaebb6d4b49c1f54afe613735241e1f0100c85c0
yeab30 <yeabsirabeletengu@gmail.com>
2023-10-13 13:38:43 +0300

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+ #include "main.h"
+/**
+ *_isdigit - check if a letter is upper
+ * @x: the number to be checked
+ * Return: 1 for upper letter or 0 for any else
+ **/
+int _isdigit(int x)
+{
+	if (x >= 48 && x <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}
