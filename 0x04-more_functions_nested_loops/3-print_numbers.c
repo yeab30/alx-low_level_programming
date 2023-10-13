b906862a4810928e59ce85fcd62e6c68abd7392f
yeab30 <yeabsirabeletengu@gmail.com>
2023-10-13 14:12:53 +0300

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+nclude "main.h"
+/**
+ * print_number - print the no since o up to 9
+ * Return: the no since 0 - 9
+ */
+void print_numbers(void)
+{
+	    int x;
+	        for (x = 0; x <= 9; x++)
+			    {
+				            _putchar(x + '0');
+					        }
+		    _putchar('\n');
+}
+
