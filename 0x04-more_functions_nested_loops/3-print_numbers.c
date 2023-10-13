dadcd29acae5a5904bb7620d5c62aa9a72a6f8c4
yeab30 <yeabsirabeletengu@gmail.com>
2023-10-13 14:21:12 +0300

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,17 @@
 #include "main.h"
+
 /**
- * print_number - print the no since o up to 9
- * Return: the no since 0 - 9
+ * print_numbers - print the no since o up to 9
+ * Return: the no since 0 up to 9
  */
 void print_numbers(void)
 {
-	    int x;
-	        for (x = 0; x <= 9; x++)
-			    {
-				            _putchar(x + '0');
-					        }
-		    _putchar('\n');
+int x;
+
+ for (x = 0; x <= 9; x++)
+ {
+ _putchar(x + '0');
+ }
+ _putchar('\n');
 }
 
