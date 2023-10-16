3dcf43456da1ab32075a627ac0cb0dce127b7979
yeab30 <yeabsirabeletengu@gmail.com>
2023-10-17 00:10:21 +0300

    d

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints n
+ * @a: array of integer
+ * @n: number 
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int j;
+	for (j = 0; j < n; j++)
+	{
+		printf("%d", a[j]);
+		if (j != (n - 1))
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
