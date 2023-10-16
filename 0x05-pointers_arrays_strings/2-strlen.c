869f8ebf2d96784bd589799844f751d77c0a61b3
yeab30 <yeabsirabeletengu@gmail.com>
2023-10-16 22:24:28 +0300

    d

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-int _strlen(char *s) {
+int _strlen(char *s)
+/*ad*/
+{
 	    int len = 0;
 	        while (*s != '\0') {
 			        len++;
 				        s++;
 					    }
 		    return len;
+
 }
