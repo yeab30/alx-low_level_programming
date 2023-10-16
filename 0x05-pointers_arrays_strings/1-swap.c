2de0a66d155e37c6d0491cbd24a6155278677f09
yeab30 <yeabsirabeletengu@gmail.com>
2023-10-16 22:10:34 +0300

    s

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * swap_int - swap
+ * @a: j
+ * @b: f
+ * Return: nothing
+ */
+void swap_int(int *a, int *b) 
+/* the function*/
+{
+	    int temp = *a;
+	    *a = *b;
+	    *b = temp;
+}
+
+ 
