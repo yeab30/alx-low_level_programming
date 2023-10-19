a73192f01a1263f11f10cf188208931bc809cb87
yeab30 <yeabsirabeletengu@gmail.com>
2023-10-19 23:25:23 +0300

    f

--- 0x06-pointers_arrays_strings/103-main.c
+++ 0x06-pointers_arrays_strings/103-main.c
@@ -1,59 +1,47 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- *     */
-int main(void)
-{
-	        char *n = "1234567892434574367823574575678477685785645685876876774586734734563456453743756756784458";
-		        char *m = "9034790663470697234682914569346259634958693246597324659762347956349265983465962349569346";
-			        char r[100];
-				        char r2[10];
-					        char r3[11];
-						        char *res;
+ * infinite_add - add two numbers.
+ *
+ * @n1: first number.
+ * @n2: second number.
+ * @r: result.
+ * @size_r: result size.
+ * Return: the addition of n1 and n2.
+ */
 
-							        res = infinite_add(n, m, r, 100);
-								        if (res == 0)
-										        {
-												                printf("Error\n");
-														        }
-									        else
-											        {
-													                printf("%s + %s = %s\n", n, m, res);
-															        }
-										        n = "1234567890";
-											        m = "1";
-												        res = infinite_add(n, m, r2, 10);
-													        if (res == 0)
-															        {
-																	                printf("Error\n");
-																			        }
-														        else
-																        {
-																		                printf("%s + %s = %s\n", n, m, res);
-																				        }
-															        n = "999999999";
-																        m = "1";
-																	        res = infinite_add(n, m, r2, 10);
-																		        if (res == 0)
-																				        {
-																						                printf("Error\n");
-																								        }
-																			        else
-																					        {
-																							                printf("%s + %s = %s\n", n, m, res);
-																									        }
-																				        res = infinite_add(n, m, r3, 11);
-																					        if (res == 0)
-																							        {
-																									                printf("Error\n");
-																											        }
-																						        else
-																								        {
-																										                printf("%s + %s = %s\n", n, m, res);
-																												        }
-																							        return (0);
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int add = 0, len1, len2, i, j;
+
+	for (len1 = 0; n1[len1]; len1++)
+	;
+	for (len2 = 0; n2[len2]; len2++)
+	;
+	if (len1 > size_r || len2 > size_r)
+	return (0);
+	len1--;
+	len2--;
+	size_r--;
+	for (i = 0; i < size_r; i++, len1--, len2--)
+	{
+		if (len1 >= 0)
+			add += n1[len1] - '0';
+		if (len2 >= 0)
+			add += n2[len2] - '0';
+		if (len1 < 0 && len2 < 0 && add == 0)
+			break;
+		r[i] = add % 10 + '0';
+		add /= 10;
+	}
+	r[i] = '\0';
+	if (len1 >= 0 || len2 >= 0 || add)
+		return (0);
+	for (i--, j = 0; i > j; i--, j++)
+	{
+		add = r[i];
+		r[i] = r[j];
+		r[j] = add;
+	}
+	return (r);
 }
